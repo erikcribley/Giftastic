f884a4e23b4781b2ff627c588b6694174c9c91b4
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-17 02:13:17 -0400

    gif buttons display gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,9 +14,13 @@ let topics = [
 
 //loop to generate topic buttons
 function makeBtn () {
+    $('#btn-display').empty()
     for(i = 0; i < topics.length; i++) {
     let gifBtn = $('<button>')
-    gifBtn.text(topics[i])
+    gifBtn
+        .addClass("gif-btn")
+        .text(topics[i])
+        .attr("data-gif", topics[i])
     $('#btn-display').append(gifBtn)
     }
 }
@@ -24,20 +28,27 @@ function makeBtn () {
 //call makeBtn function
 makeBtn()
 
-//make a new button
-function newBtn() {
-    $('#btn-display').empty()
-    makeBtn()
-}
-
 //click event to create new topic button
 $("#search-submit").on("click", function(event){
     event.preventDefault()
-    let searchFor = $("#search-input").val()
+    let searchFor = $("#search-input").val().trim()
     topics.push(searchFor)
-    newBtn()
+    makeBtn()
 })
 
-
-let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + "x" + "&api_key=DNjwWLOHXOEZm2mWllD1dgkcS3sEmUh4&limit=10&rating=pg-13"
-
+$('button').on("click", function (){
+    let getGif = $(this).attr("data-gif")
+    let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + getGif + "&api_key=DNjwWLOHXOEZm2mWllD1dgkcS3sEmUh4&limit=10&rating=pg-13"
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        let rating = response.data[0].rating
+        let imageURL = response.data[0].images.original.url
+        let image = $("<img>")
+        image.attr("src", imageURL)
+        image.attr("alt", "cute gif")
+        $("#gif-display").prepend(image)        
+    })
+})
