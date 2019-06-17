b87e50c08685fdc42e0b37ecd4a401e550380039
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-16 21:38:16 -0400

    created buttons from array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,26 @@
 //api key DNjwWLOHXOEZm2mWllD1dgkcS3sEmUh4
+
+let topics = [
+    "kitties",
+    "doge",
+    "octopi",
+    "tardigrades",
+    "parrots",
+    "robots",
+    "snakes",
+    "giraffes",
+    "lizards",
+    "unicorns",
+]
+
+for(i = 0; i < topics.length; i++) {
+    let gifBtn = $('<button>')
+    gifBtn.text(topics[i])
+    $('#btn-display').append(gifBtn)
+}
+
+let searchFor = $('#search-input').val()
+
+let queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchFor + "&api_key=DNjwWLOHXOEZm2mWllD1dgkcS3sEmUh4&limit=10&rating=pg-13"
+
+console.log(searchFor)
